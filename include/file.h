166d95caaea118f660537cf5c885043a8cf3eca0
Dedeclear <dnssabau@gmail.com>
2025-07-03 17:02:26 +0200

    Implement function to load wine collection from .csv

--- include/file.h
+++ include/file.h
@@ -3,11 +3,20 @@
 
 #include "wines.h"
 
+#define MAX_LINE_LEN 370
+
 /**
  * @brief allows saving of wines collection on .csv file
  * @param collection struct containing array of Wine pointers and number of Wines saved
  * @param filename path to the .csv file
  */
 void save_on_csv(WineCollection* collection, const char* filename);
+
+/**
+ * @brief allows loading of wines collection from .csv file
+ * @param collection struct containing array of Wine pointers and number of Wines saved
+ * @param filename path to the .csv file
+ */
+void load_from_csv(WineCollection* collection, const char* filename);
 
 #endif
--- src/file.c
+++ src/file.c
@@ -29,3 +29,57 @@ void save_on_csv(WineCollection* collection, const char* filename){
 
     fclose(f);
 }
+
+void load_from_csv(WineCollection* collection, const char* filename){
+    if(!filename || !collection){
+        perror("Invalid filename or WineCollection pointer, please try again.\n");
+        return;
+    }
+
+    FILE* f = fopen(filename, "r");
+    if(!f){
+        perror("Error: unable to open file in read mode.\n");
+        return;
+    }
+
+    char line[MAX_LINE_LEN];
+
+    //skip first line
+    fgets(line, sizeof(line), f);
+
+    //now file pointer is on second line
+    //loop to read all saved wines
+    while(fgets(line, sizeof(line), f) != NULL){
+        char* token;
+        Wine* new_wine = malloc(sizeof(Wine));
+        if(!new_wine){
+            perror("Error: failed to allocate memory for new wine");
+            break;
+        }
+        //we find first '\n' occurency and change it with '\0'
+        line[strcspn(line, "\n")] = '\0';
+
+        token = strtok(line, ",");
+        if(token) strcpy(new_wine->name, token);
+
+        //now instead of line we give NULL in order to continue reading the same line
+        token = strtok(NULL, ",");
+        if(token) strcpy(new_wine->type, token);
+
+        token = strtok(NULL, ",");
+        if(token) strcpy(new_wine->wine_variety, token);
+
+        token = strtok(NULL, ",");
+        if(token) new_wine->prod_year = atoi(token);
+
+        token = strtok(NULL, ",");
+        if(token) new_wine->mark = atoi(token);
+
+        token = strtok(NULL, ",");
+        if(token) strcpy(new_wine->optional_notes, token);
+
+        collection->wines[collection->count] = new_wine;
+        collection->count++;
+    }
+
+}
